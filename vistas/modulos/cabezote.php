a40056e576b5c331fac3e8d6dd0c5c5bc6439fd3
Arturo <gregory09ruiz@gmail.com>
2020-08-31 16:33:19 -0400

    cabezote

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -3,7 +3,7 @@
 	<!--=====================================
 	LOGOTIPO
 	======================================-->
-    <a href="escritorio.php" class="logo">
+    <a href="#" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>SIS</b> V</span>
       <!-- logo for regular state and mobile devices -->
